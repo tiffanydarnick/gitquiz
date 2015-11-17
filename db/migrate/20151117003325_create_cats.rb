0945a3971864a56b856d52b0d71aadf61f621e78
tiffanydarnick <tdarnick@outlook.com>
2015-11-17 00:49:06 +0000

    color age

--- db/migrate/20151117003325_create_cats.rb
+++ db/migrate/20151117003325_create_cats.rb
@@ -2,6 +2,8 @@ class CreateCats < ActiveRecord::Migration
   def change
     create_table :cats do |t|
     	t.string :name
+    	t.string :color
+    	t.integer :age
 
       	t.timestamps
     end
